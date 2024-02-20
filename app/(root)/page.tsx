b7731a333c19437d995584f92df520bf51113dca
Kent <2021node@gmail.com>
2024-02-20 20:55:07 +0800

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,22 @@
-import { Collection } from "@/components/shared/Collection"
-import { navLinks } from "@/constants"
-import { getAllImages } from "@/lib/actions/image.actions"
-import Image from "next/image"
-import Link from "next/link"
+import { Collection } from '@/components/shared/Collection';
+import { navLinks } from '@/constants';
+import { getAllImages } from '@/lib/actions/image.actions';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
-  const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery });
 
   return (
     <>
       <section className="home">
-        <h1 className="home-heading">
-          Unleash Your Creative Vision with Imaginify
-        </h1>
+        <h1 className="home-heading">Unleash Your Creative Vision with Envisage</h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
-            <Link
-              key={link.route}
-              href={link.route}
-              className="flex-center flex-col gap-2"
-            >
+            <Link key={link.route} href={link.route} className="flex-center flex-col gap-2">
               <li className="flex-center w-fit rounded-full bg-white p-4">
                 <Image src={link.icon} alt="image" width={24} height={24} />
               </li>
@@ -33,15 +27,10 @@ const Home = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="sm:mt-12">
-        <Collection 
-          hasSearch={true}
-          images={images?.data}
-          totalPages={images?.totalPage}
-          page={page}
-        />
+        <Collection hasSearch={true} images={images?.data} totalPages={images?.totalPage} page={page} />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
